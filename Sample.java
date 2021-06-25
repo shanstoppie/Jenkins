0771048e3ed113f4514cd5f420ef0a704a3b7743
vettrivikas <32515723+vettrivikas@users.noreply.github.com>
2021-06-25 14:29:17 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,5 +3,6 @@ class JenkinExample {
 public static void main(String args[]){
 System.out.println("Starting with Jenkins:");
 System.out.println("******welcome to automation******");
+System.out.println("******welcome to automation******");
   }
 }
